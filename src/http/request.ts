e2996f836ed11b61c5b5e8a62bf37d18ac1c647b
dawei <dawei@163.com>
2023-11-12 20:46:03 +0800

    feat: 搭建工程

---
+++ src/http/request.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const service = axios.create({
+    baseURL: import.meta.env.VITE_APP_BASE_API,
+    timeout: 15000
+});
+// axios实例拦截请求
+service.interceptors.request.use(config => {
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+// axios实例拦截响应
+service.interceptors.response.use(response => {
+    return response.data;
+} , error => {} );
+export default service;
---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+// 配置路由
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('../views//home/index.vue'),
+        meta: {},
+        children: [],
+    },
+];
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+router.beforeEach(async (_to, _from, next) => {
+    NProgress.start();
+    next();
+});
+router.afterEach((_to) => {
+    NProgress.done();
+});
+export default router;
